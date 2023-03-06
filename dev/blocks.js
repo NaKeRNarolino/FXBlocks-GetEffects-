6d6a20f3b68ea4539ff142fd801e986baa266a31
NaKeR <122722654+NaKeRNarolino@users.noreply.github.com>
2023-03-06 21:13:03 +0400

    Update 1.1

--- dev/blocks.js
+++ dev/blocks.js
@@ -4,6 +4,10 @@ IDRegistry.genBlockID("RegenBLK");
 IDRegistry.genBlockID("FlyBLK");
 IDRegistry.genBlockID("NightVBLK");
 IDRegistry.genBlockID("AbsorptionBLK");
+IDRegistry.genBlockID("HasteBLK");
+IDRegistry.genBlockID("SaturBLK");
+IDRegistry.genBlockID("JumpBBLK");
+IDRegistry.genBlockID("StrengthBLK");
 
 Block.createSpecialType(
     {
@@ -63,6 +67,10 @@ FxBlock("RegenBLK", "Regeneration", "regen_blk");
 FxBlock("FlyBLK", "Levitation", "fly_blk");
 FxBlock("NightVBLK", "Night Vision", "nightv_blk");
 FxBlock("AbsorptionBLK", "Absorption", "absorp_blk");
+FxBlock("HasteBLK", "Haste", "haste_blk");
+FxBlock("SaturBLK", "Saturation", "satur_blk");
+FxBlock("JumpBBLK", "Jump Boost", "jumpb_blk");
+FxBlock("StrengthBLK", "Strength", "strength_blk");
 
 clckFxBlock("InvisibilityBLK", "invisibility");
 clckFxBlock("SpeedBLK", "speed");
@@ -70,6 +78,10 @@ clckFxBlock("RegenBLK", "regeneration");
 clckFxBlock("FlyBLK", "levitation");
 clckFxBlock("NightVBLK", "night_vision");
 clckFxBlock("AbsorptionBLK", "absorption");
+clckFxBlock("HasteBLK", "haste");
+clckFxBlock("SaturBLK", "saturation");
+clckFxBlock("JumpBBLK", "jump_boost");
+clckFxBlock("StrengthBLK", "strength");
 
 recFxBlock(BlockID.InvisibilityBLK, VanillaItemID.iron_ingot);
 recFxBlock(BlockID.SpeedBLK, VanillaBlockID.redstone_block);
@@ -77,3 +89,7 @@ recFxBlock(BlockID.RegenBLK, VanillaItemID.gold_ingot);
 recFxBlock(BlockID.FlyBLK, VanillaItemID.diamond);
 recFxBlock(BlockID.NightVBLK, VanillaItemID.iron_nugget);
 recFxBlock(BlockID.AbsorptionBLK, VanillaItemID.emerald);
+recFxBlock(BlockID.HasteBLK, VanillaBlockID.diamond_block);
+recFxBlock(BlockID.SaturBLK, VanillaItemID.golden_apple);
+recFxBlock(BlockID.JumpBBLK, VanillaItemID.rabbit_foot);
+recFxBlock(BlockID.StrengthBLK, VanillaItemID.diamond_sword);
